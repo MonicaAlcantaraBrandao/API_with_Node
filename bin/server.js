1d106965fafaba0de665d9cc8ee09cb57b5a1e05
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-10-18 20:21:32 -0300

    separete server

--- bin/server.js
+++ bin/server.js
@@ -1,23 +1,12 @@
-'use strict'
-
 const http = require('http');
 const debug = require('debug');
-const express = require('express');
 
-const app = express();
+const app = require('../src/app');
+
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError)
