791db5e23962e83b7a15d6a1936fa92fae274d4c
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-10-18 20:04:50 -0300

    add debug

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError)
+server.on('listening', onListening)
+
 console.log("Servidor rodando na porta " + port)
 
 function normalizePort(val){
@@ -57,4 +60,10 @@ function onError(error){
         default:
             throw error;
     }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind)
 }
